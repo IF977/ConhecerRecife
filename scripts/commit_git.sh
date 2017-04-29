787870c877d61cd29bf4a34a81eb786126a0ed3d
Diogo Vieira <dvd@cin.ufpe.br>
2017-04-29 22:05:08 +0000

    adicionando mais historias de usuarios

--- scripts/commit_git.sh
+++ scripts/commit_git.sh
@@ -1,3 +1,13 @@
+enviar arquivos para git:
+    
 git add -A
 git commit -m "configurando projeto com as ferramentas que serão utilizadas"
 git push -u origin master
+
+
+
+
+
+atualizar heroku:
+
+git push heroku master
