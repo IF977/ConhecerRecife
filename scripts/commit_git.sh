7152330a963886558b56843232a1fe6c4bbce142
Diogo Vieira <dvd@cin.ufpe.br>
2017-04-30 23:37:32 +0000

    inicio do desenvolvimento

--- scripts/commit_git.sh
+++ scripts/commit_git.sh
@@ -1,23 +1,26 @@
 enviar arquivos para git:
     
 git add -A
-git commit -m "configurando projeto com as ferramentas que serão utilizadas"
+git commit -m "inicio do desenvolvimento"
 git push -u origin master
-
-
-
 
 
 atualizar heroku:
 
 git push heroku master
+heroku run rake db:migrate
+heroku run rake db:seed
+heroku open
 
 
 atualizando nome
 git remote rm heroku
 heroku git:remote -a conhecer-recife
 
+
 rails g scaffold Useranonimo local:string
 
 
 sudo -u postgres createuser -rds conhecerrecife
+CREATE DATABASE "nomedobanco";
+
