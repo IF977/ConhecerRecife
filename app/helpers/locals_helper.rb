c4c9fa974ca59b492e3127b29d9710c004fb6226
Tiago Costa <tiakkost@gmail.com>
2017-06-26 18:19:04 +0000

    cadastro de outros locais

--- app/helpers/locals_helper.rb
+++ app/helpers/locals_helper.rb
@@ -1,2 +1,11 @@
 module LocalsHelper
+    
+    def imagem(local)
+        
+        if local.imagem.blank?
+            image_tag('sem-imagem.jpg')
+        else
+            image_tag(local.imagem)
+        end
+    end
 end
